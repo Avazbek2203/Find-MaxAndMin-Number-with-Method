b68e9e3913c62ac8c0fc5e32e230b42382d3960a
Avazbek2203 <128214329+Avazbek2203@users.noreply.github.com>
2023-04-05 11:47:54 +0500

    Add project files.

---
+++ Arrays 2.3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double number1 = GetNumberInput();
+        double number2 = GetNumberInput();
+        double result  = (CheckNumbers(number1, number2));
+        Console.WriteLine($"The larger number is {result}");
+
+    }
+    public static double CheckNumbers(double number1 , double number2)
+    {
+        if(number1 > number2) {
+            return number1;
+        }
+        else
+        {
+            return number2;
+        }
+    }
+
+
+    public static double GetNumberInput()
+    {
+        Console.Write("Enter a number : ");
+        double number;
+        while (!double.TryParse(Console.ReadLine(), out number))
+        {
+            Console.WriteLine("Please enter a valid number!");
+            Console.Write("Enter number : ");
+        }
+
+        return number;
+
+
+    }
+}
